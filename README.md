e30f3bc4b35e41e70089f6fda7ffc528d05a9ba9
Sona Keshishyan <sonakeshishyan@Sonas-MacBook-Pro.local>
2024-06-02 15:49:36 -0700

    updates

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 References:
 https://www.w3schools.com/css/css3_buttons.asp
 https://www.w3schools.com/css/css_link.asp
+https://www.youtube.com/watch?v=sNXfI3woBEw&ab_channel=JohnKomarnicki
 https://www.w3schools.com/css/css3_buttons.asp
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*html"
+
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "main-pink": "#FFA7EC",
+      }
+    },
+    fontFamily: {
+      Kameron: ["Kameron, sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
